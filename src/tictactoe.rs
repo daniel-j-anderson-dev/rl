58ab1ca834a92ad5c4044a17e9e826e400c5fd1b
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2026-01-16 17:06:35 -0700

    added test to call train

--- src/tictactoe.rs
+++ src/tictactoe.rs
@@ -13,6 +13,21 @@ pub use self::{
     model::*,
 };
 
+#[test]
+fn train_tic_tac_toe_model() {
+    use burn::{
+        backend::{Autodiff, Wgpu},
+        optim::AdamConfig,
+    };
+
+    train::<Autodiff<Wgpu>>(
+        "./artifacts",
+        TrainingConfig::new(TicTacToeNetworkConfig::new(), AdamConfig::new(), Player::X),
+        Default::default(),
+    )
+    .unwrap();
+}
+
 pub const ROW_COUNT: usize = 3;
 pub const COLUMN_COUNT: usize = 3;
 pub const PLAYER_COUNT: usize = 2;
